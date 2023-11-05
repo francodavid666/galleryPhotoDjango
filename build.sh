854af00248417fb8f2c105dd1c8b6927c4c3a669
franco66 <david_franco@outlook.com>
2023-11-05 15:42:51 -0300

    versel5

---
+++ build.sh
@@ -0,0 +1,16 @@
+
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+
+echo "Make migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
+
+
+
